a596c1bc2d5fc1ab741894bea071adb4820a8eee
DOCraven <brg.tman@gmail.com>
2020-02-27 15:59:49 +1100

    S4P work, new str msg

--- test.py
+++ test.py
@@ -7,3 +7,8 @@ print(msg)
 newmsg = '\nDownloaded off SP4, added this on HP ELITE, uploading to git'
 
 print(newmsg)
+
+finalmsg = '\nI think this is working, did this work on my S4P'
+
+print(finalmsg)
+
